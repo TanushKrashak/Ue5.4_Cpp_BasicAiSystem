f7301e93bbc0129aa62ee504b734a12e7fd0a18e
TanushKrashak <tankrashak38816@gmail.com>
2024-05-29 23:46:31 +0530

    Implem of Cpp_BTTask_ChasePlayer::ExecuteTask()

--- Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_ChasePlayer.cpp
+++ Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_ChasePlayer.cpp
@@ -9,5 +9,17 @@ UCpp_BTTask_ChasePlayer::UCpp_BTTask_ChasePlayer(const FObjectInitializer& Objec
 }
 
 EBTNodeResult::Type UCpp_BTTask_ChasePlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	
+	// Get target location from blackboard via NPC's controller
+	if (const auto* AiCont = Cast<ACpp_AiC_NPC>(OwnerComp.GetOwner())) {
+		// Get player location from blackboard
+		const auto PlayerLocation = OwnerComp.GetBlackboardComponent()->GetValueAsVector(GetSelectedBlackboardKey());
+
+		// Move to player location
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(AiCont, PlayerLocation);
+
+		// Finish with success
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return EBTNodeResult::Succeeded;
+	}
+	return EBTNodeResult::Failed;
 }
