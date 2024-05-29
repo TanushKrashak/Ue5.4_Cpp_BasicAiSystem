ab6ae161cbf387f352204a82d170c2bbbf7dbedb
TanushKrashak <tankrashak38816@gmail.com>
2024-05-29 20:02:48 +0530

    Implem of Cpp_AiC_NPC::SetupPerceptionSystem()

--- Source/Cpp_3dAiSystem/Private/NPC/Cpp_AiC_NPC.cpp
+++ Source/Cpp_3dAiSystem/Private/NPC/Cpp_AiC_NPC.cpp
@@ -6,9 +6,10 @@
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AIPerceptionComponent.h"
 
 ACpp_AiC_NPC::ACpp_AiC_NPC(FObjectInitializer const& ObjectInitializer) {
-
+	SetupPerceptionSystem();
 }
 
 void ACpp_AiC_NPC::OnPossess(APawn* InPawn) {
@@ -25,7 +26,24 @@ void ACpp_AiC_NPC::OnPossess(APawn* InPawn) {
 }
 
 void ACpp_AiC_NPC::SetupPerceptionSystem() {
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight Config"));
+	if (SightConfig) {
+		SetPerceptionComponent(*CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("Perception Component")));
+		SightConfig->SightRadius = 500.0f;
+		SightConfig->LoseSightRadius = SightConfig->SightRadius + 50.0f;
+		SightConfig->PeripheralVisionAngleDegrees = 90.0f;
+		// Time in seconds after which the target is considered lost if not seen
+		SightConfig->SetMaxAge(5.0f);
+		SightConfig->AutoSuccessRangeFromLastSeenLocation = 520.0f;
+		SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+		SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+		SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
 
+		GetPerceptionComponent()->SetDominantSense(*SightConfig->GetSenseImplementation());
+		// Call the OnTargetDetected function when the target is detected
+		GetPerceptionComponent()->OnTargetPerceptionUpdated.AddDynamic(this, &ACpp_AiC_NPC::OnTargetDetected);
+		GetPerceptionComponent()->ConfigureSense(*SightConfig);
+	}
 }
 
 void ACpp_AiC_NPC::OnTargetDetected(AActor* Actor, FAIStimulus const Stimulus) {
