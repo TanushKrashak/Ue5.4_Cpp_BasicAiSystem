24d0f586d12409926a3a5cf9116cedeee58c2381
TanushKrashak <tankrashak38816@gmail.com>
2024-05-29 19:23:41 +0530

    Implem of UCpp_BTTask_FindRandomLocation::ExecuteTask()

--- Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_FindRandomLocation.cpp
+++ Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_FindRandomLocation.cpp
@@ -2,8 +2,35 @@
 
 
 #include "Tasks/Cpp_BTTask_FindRandomLocation.h"
+#include "NPC/Cpp_AiC_NPC.h"
+#include "NavigationSystem.h"
+
+UCpp_BTTask_FindRandomLocation::UCpp_BTTask_FindRandomLocation(FObjectInitializer const& ObjectInitializer) {
+	// the NodeName is the name of the task that will be displayed in the Behavior Tree Editor
+	NodeName = TEXT("Find Random Location");
+}
 
 EBTNodeResult UCpp_BTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	Super::ExecuteTask(OwnerComp, NodeMemory);
-	
+	// get the AI controller and the controlled pawn
+	if (const auto* AiController = Cast<ACpp_AiC_NPC>(OwnerComp.GetAIOwner())) {
+		if (const auto* Npc = AiController->GetPawn()) {
+			// get npc location as origin for the random location
+			const auto Origin = Npc->GetActorLocation();
+
+			// get the navigation system and generate a random location
+			if (const auto* NavSys = UNavigationSystemV1::GetCurrent(GetWorld())) {
+				FNavLocation ResultLocation;
+
+				if (NavSys->GetRandomPointInNavigableRadius(Origin, SearchRadius, ResultLocation)) {
+					// write the result location to the blackboard
+					OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), ResultLocation.Location);
+				}
+				// Finish with success
+				FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+				return EBTNodeResult::Succeeded;
+			}
+		}
+	}
+	// Finish with failure
+	return EBTNodeResult::Failed;
 }
--- Source/Cpp_3dAiSystem/Public/Tasks/Cpp_BTTask_FindRandomLocation.h
+++ Source/Cpp_3dAiSystem/Public/Tasks/Cpp_BTTask_FindRandomLocation.h
@@ -14,7 +14,11 @@ class CPP_3DAISYSTEM_API UCpp_BTTask_FindRandomLocation : public UBTTask_Blackbo
 {
 	GENERATED_BODY()
 	
-
+public:
+	explicit UCpp_BTTask_FindRandomLocation(FObjectInitializer const& ObjectInitializer);
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ai", meta = (AllowPrivateAccess = true))
+	float SearchRadius = 1500.0f;
 };
