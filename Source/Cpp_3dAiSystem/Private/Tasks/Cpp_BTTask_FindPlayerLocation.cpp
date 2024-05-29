83586c8c8ffc1584d8808ec8ef16ffb9dfc7b2b6
TanushKrashak <tankrashak38816@gmail.com>
2024-05-29 19:52:56 +0530

    Implem Of Cpp_BTTask_FindPlayerLocation::ExecuteTask()

--- Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_FindPlayerLocation.cpp
+++ Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_FindPlayerLocation.cpp
@@ -2,7 +2,39 @@
 
 
 #include "Tasks/Cpp_BTTask_FindPlayerLocation.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "NavigationSystem.h"
+#include "GameFramework/Character.h"
 
-EBTNodeResult UCpp_BTTask_FindPlayerLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	Super::ExecuteTask(OwnerComp, NodeMemory);	
+
+UCpp_BTTask_FindPlayerLocation::UCpp_BTTask_FindPlayerLocation(FObjectInitializer const& ObjectInitializer) {
+	// NodeName is the name of the task that will be displayed in the Behavior Tree Editor
+	NodeName = TEXT("Find Player Location");
+}
+
+EBTNodeResult::Type UCpp_BTTask_FindPlayerLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+	// Get Player Character
+	if (const auto* Player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)) {
+		// Get the player's location
+		const auto PlayerLocation = Player->GetActorLocation();
+		if (bRandLocAroundPlayer) {
+			FNavLocation NavLocation;
+			// Get the navigation system and generate a random location around the player
+			if (const auto* NavSys = UNavigationSystemV1::GetCurrent(GetWorld())) {
+				// Try to get a random point in the navigable radius around the player
+				if (NavSys->GetRandomPointInNavigableRadius(PlayerLocation, SearchRadius, NavLocation)) {
+					OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), NavLocation.Location);
+				}
+			}
+		}
+		else {
+			OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), PlayerLocation);			
+		}
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return EBTNodeResult::Succeeded;
+	}
+	
+	return EBTNodeResult::Failed;	
 }
--- Source/Cpp_3dAiSystem/Public/Tasks/Cpp_BTTask_FindPlayerLocation.h
+++ Source/Cpp_3dAiSystem/Public/Tasks/Cpp_BTTask_FindPlayerLocation.h
@@ -6,12 +6,21 @@
 #include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
 #include "Cpp_BTTask_FindPlayerLocation.generated.h"
 
-/**
- * 
- */
+class UBehaviorTreeComponent;
+
 UCLASS()
 class CPP_3DAISYSTEM_API UCpp_BTTask_FindPlayerLocation : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
 	
+public:
+	explicit UCpp_BTTask_FindPlayerLocation(FObjectInitializer const& ObjectInitializer);
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Search", meta = (AllowPrivateAccess = true))
+	bool bRandLocAroundPlayer = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Search", meta = (AllowPrivateAccess = true))
+	float SearchRadius = 1500.0f;
 };
