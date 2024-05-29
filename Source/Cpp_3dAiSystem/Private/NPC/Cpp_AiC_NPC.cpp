4e92b4222e58dea7ad44a319c7417bcd1d79e597
TanushKrashak <tankrashak38816@gmail.com>
2024-05-29 16:12:55 +0530

    Created AiC_NPC class (Ai controller)

---
+++ Source/Cpp_3dAiSystem/Private/NPC/Cpp_AiC_NPC.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPC/Cpp_AiC_NPC.h"
+
---
+++ Source/Cpp_3dAiSystem/Public/NPC/Cpp_AiC_NPC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Cpp_AiC_NPC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_3DAISYSTEM_API ACpp_AiC_NPC : public AAIController
+{
+	GENERATED_BODY()
+	
+};
