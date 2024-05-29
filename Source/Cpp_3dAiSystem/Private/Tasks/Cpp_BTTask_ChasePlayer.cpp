e8009dea878c1daff63bbc64726d61d45c725475
TanushKrashak <tankrashak38816@gmail.com>
2024-05-29 23:53:33 +0530

    Compile Errors Fixed

--- Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_ChasePlayer.cpp
+++ Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_ChasePlayer.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Tasks/Cpp_BTTask_ChasePlayer.h"
+#include "NPC/Cpp_AiC_NPC.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
 
 UCpp_BTTask_ChasePlayer::UCpp_BTTask_ChasePlayer(const FObjectInitializer& ObjectInitializer) {
 	// NodeNames are names which are shown in the BehaviorTree Editor
@@ -10,7 +13,7 @@ UCpp_BTTask_ChasePlayer::UCpp_BTTask_ChasePlayer(const FObjectInitializer& Objec
 
 EBTNodeResult::Type UCpp_BTTask_ChasePlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
 	// Get target location from blackboard via NPC's controller
-	if (const auto* AiCont = Cast<ACpp_AiC_NPC>(OwnerComp.GetOwner())) {
+	if (auto* AiCont = Cast<ACpp_AiC_NPC>(OwnerComp.GetAIOwner())) {
 		// Get player location from blackboard
 		const auto PlayerLocation = OwnerComp.GetBlackboardComponent()->GetValueAsVector(GetSelectedBlackboardKey());
 
--- Source/Cpp_3dAiSystem/Public/Tasks/Cpp_BTTask_ChasePlayer.h
+++ Source/Cpp_3dAiSystem/Public/Tasks/Cpp_BTTask_ChasePlayer.h
@@ -6,12 +6,16 @@
 #include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
 #include "Cpp_BTTask_ChasePlayer.generated.h"
 
-/**
- * 
- */
+// Forward Declarations
+class UBehaviorTreeComponent;
+
 UCLASS()
 class CPP_3DAISYSTEM_API UCpp_BTTask_ChasePlayer : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
 	
+public:
+	explicit UCpp_BTTask_ChasePlayer(const FObjectInitializer& ObjectInitializer);
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
 };
