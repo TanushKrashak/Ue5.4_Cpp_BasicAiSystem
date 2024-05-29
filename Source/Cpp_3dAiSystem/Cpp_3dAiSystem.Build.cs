8f58e93d75e3565f381a8a721432398434800920
TanushKrashak <tankrashak38816@gmail.com>
2024-05-29 16:13:03 +0530

    Added Required Linkers

--- Source/Cpp_3dAiSystem/Cpp_3dAiSystem.Build.cs
+++ Source/Cpp_3dAiSystem/Cpp_3dAiSystem.Build.cs
@@ -8,6 +8,15 @@ public class Cpp_3dAiSystem : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+			"AIModule", 
+			"GameplayTasks", 
+			"NavigationSystem"
+		});
 	}
 }
