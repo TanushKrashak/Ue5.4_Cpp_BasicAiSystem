e3d299804ec582e46d3590b29ba5863d65bb3527
TanushKrashak <tankrashak38816@gmail.com>
2024-05-30 22:40:52 +0530

    Creation of Cpp_BTTask_MeleeAttack

---
+++ Source/Cpp_3dAiSystem/Public/Services/Cpp_BTS_IsPlayerInMeleeRange.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "Cpp_BTS_IsPlayerInMeleeRange.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_3DAISYSTEM_API UCpp_BTS_IsPlayerInMeleeRange : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Cpp_3dAiSystem/Public/Tasks/Cpp_BTTask_MeleeAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "Cpp_BTTask_MeleeAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_3DAISYSTEM_API UCpp_BTTask_MeleeAttack : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
