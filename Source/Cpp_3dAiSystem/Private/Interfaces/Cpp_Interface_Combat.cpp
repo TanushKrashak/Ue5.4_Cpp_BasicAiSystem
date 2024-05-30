25662cedb42604bf0c088b618e34604881983293
TanushKrashak <tankrashak38816@gmail.com>
2024-05-30 22:28:09 +0530

    Creation Of Cpp_Interface_Combat

---
+++ Source/Cpp_3dAiSystem/Private/Interfaces/Cpp_Interface_Combat.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/Cpp_Interface_Combat.h"
+
+// Add default functionality here for any ICpp_Interface_Combat functions that are not pure virtual.
---
+++ Source/Cpp_3dAiSystem/Public/Interfaces/Cpp_Interface_Combat.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Cpp_Interface_Combat.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UCpp_Interface_Combat : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class CPP_3DAISYSTEM_API ICpp_Interface_Combat
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Combat")
+	int MeleeAttack();
+
+};
