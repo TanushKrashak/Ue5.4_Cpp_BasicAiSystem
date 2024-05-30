35ce65e71e521f037b1a1e75d2ab7d568f998a0e
TanushKrashak <tankrashak38816@gmail.com>
2024-05-30 22:55:09 +0530

    Includes + Implem Of Cpp_BTTask_MeleeAttack()

--- Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_MeleeAttack.cpp
+++ Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_MeleeAttack.cpp
@@ -2,4 +2,27 @@
 
 
 #include "Tasks/Cpp_BTTask_MeleeAttack.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "NPC/Cpp_NPC.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "Interfaces/Cpp_Interface_Combat.h"
+#include "NPC/AiC_NPC.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Engine/World.h"
+#include "Engine/LatentActionManager.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Animation/AnimInstance.h"
+#include "Animation/AnimMontage.h"
 
+UCpp_BTTask_MeleeAttack::UCpp_BTTask_MeleeAttack() {
+	// NodeNames are the names of the nodes in the BehaviorTree in the editor
+	NodeName = TEXT("Melee Attack");
+}
+
+EBTNodeResult::Type UCpp_BTTask_MeleeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+
+}
+
+bool UCpp_BTTask_MeleeAttack::MontageHasFinished(const ACpp_NPC* npc) {
+
+}
