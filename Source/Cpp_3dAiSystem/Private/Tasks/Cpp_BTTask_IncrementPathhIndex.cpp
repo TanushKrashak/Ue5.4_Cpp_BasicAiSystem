29d565fcac3e09202e44c40443487f80e635cd93
TanushKrashak <tankrashak38816@gmail.com>
2024-05-30 18:23:28 +0530

    Constructor For Cpp_BTTask_IncrementPathhIndex

--- Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_IncrementPathhIndex.cpp
+++ Source/Cpp_3dAiSystem/Private/Tasks/Cpp_BTTask_IncrementPathhIndex.cpp
@@ -4,7 +4,8 @@
 #include "Tasks/Cpp_BTTask_IncrementPathhIndex.h"
 
 UCpp_BTTask_IncrementPathhIndex::UCpp_BTTask_IncrementPathhIndex(const FObjectInitializer& ObjectInitializer) : UBTTask_BlackboardBase{ObjectInitializer} {
-
+	// NodeName is the name shown in the Behavior Tree Editor
+	NodeName = TEXT("Increment Path Index");
 }
 
 EBTNodeResult::Type UCpp_BTTask_IncrementPathhIndex::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
