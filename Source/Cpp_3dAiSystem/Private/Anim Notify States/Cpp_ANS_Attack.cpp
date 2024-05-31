c6452ba7416923941ee46a3bfa8a5e227f3f58f6
TanushKrashak <tankrashak38816@gmail.com>
2024-05-31 17:27:22 +0530

    Compiles Now

--- Source/Cpp_3dAiSystem/Private/Anim Notify States/Cpp_ANS_Attack.cpp
+++ Source/Cpp_3dAiSystem/Private/Anim Notify States/Cpp_ANS_Attack.cpp
@@ -8,7 +8,7 @@
 void UCpp_ANS_Attack::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration) {
 	// Check if mesh and owner exist
 	if (MeshComp && MeshComp->GetOwner()) {
-		if (const ACpp_AiCharacter_Master* inChar = Cast<ACpp_AiCharacter_Master>(MeshComp->GetOwner())) {
+		if (ACpp_AiCharacter_Master* inChar = Cast<ACpp_AiCharacter_Master>(MeshComp->GetOwner())) {
 			inChar->AttackStart();
 		}
 	}
@@ -17,7 +17,7 @@ void UCpp_ANS_Attack::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenc
 void UCpp_ANS_Attack::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) {
 	// Check if mesh and owner exist
 	if (MeshComp && MeshComp->GetOwner()) {
-		if (const ACpp_AiCharacter_Master* inChar = Cast<ACpp_AiCharacter_Master>(MeshComp->GetOwner())) {
+		if (ACpp_AiCharacter_Master* inChar = Cast<ACpp_AiCharacter_Master>(MeshComp->GetOwner())) {
 			inChar->AttackEnd();
 		}
 	}
